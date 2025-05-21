c6d54b5d7733e3d8ff57577d70986e90f65860d3
Remint-app <remintconnect@gmail.com>
2025-05-21 14:38:29 +0300

    Add order page

---
+++ src/app/api/order/[id]/roure.ts
@@ -0,0 +1,4 @@
+import { NextRequest, NextResponse } from 'next/server';
+import clientPromise from '@/app/utils/mongodb';
+import { ObjectId } from 'mongodb';
+
--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -21,3 +21,28 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ success: false, error: 'DB error' }, { status: 500 });
   }
 }
+
+
+export async function GET(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id');
+    console.log(id)
+    if (!id) {
+      return NextResponse.json({ success: false, error: 'Missing id' }, { status: 400 });
+    }
+
+    const client = await clientPromise;
+    const db = client.db();
+    const order = await db.collection('orders').findOne({ _id: new ObjectId(id) });
+
+    if (!order) {
+      return NextResponse.json({ success: false, error: 'Order not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ success: true, order });
+  } catch (error) {
+    console.error('DB Get Error:', error);
+    return NextResponse.json({ success: false, error: 'DB error' }, { status: 500 });
+  }
+}
